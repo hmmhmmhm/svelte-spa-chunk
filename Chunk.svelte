f1b5688f72c1153825c5f5cecb853e7b143abef8
hmmhmmhm <hmmhmmhm@naver.com>
2020-02-01 20:36:08 +0900

    Fix Dynamic Import White Splash

--- Chunk.svelte
+++ Chunk.svelte
@@ -1,7 +1,28 @@
 <script>
+    import { fly } from 'svelte/transition'
     import Loadable from 'svelte-loadable'
     export let params = {}
     export let dynamicImport
+    export let delay = 0
 </script>
 
-<Loadable loader={dynamicImport} params={params} />
+<Loadable loader={dynamicImport} {params} {delay}>
+
+    <div
+        class="loadable-container"
+        out:fly={{ x: 0, duration: 4000 }}
+        slot="success"
+        let:component
+    >
+        <svelte:component this={component} {params} />
+    </div>
+
+</Loadable>
+
+<style>
+    .loadable-container {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+    }
+</style>
