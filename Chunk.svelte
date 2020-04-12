f86ed16054d1c4ca2915c3c33374b4f8204cdde6
hmmhmmhm <hmmhmmhm@naver.com>
2020-04-12 18:37:57 +0900

    Fix Issue #3

--- Chunk.svelte
+++ Chunk.svelte
@@ -2,45 +2,14 @@
     import { fly } from 'svelte/transition'
     import Loadable from 'svelte-loadable'
     import { get } from 'svelte/store'
-    import isPageChanged from './store'
     export let params = {}
     export let dynamicImport
     export let delay = 0
-
-    const flashPrevent = node => {
-        return {
-            duration: 4000,
-            tick: t => {
-                if (get(isPageChanged)) {
-                    if (node.style.display != 'none')
-                        node.style.display = 'none'
-                } else {
-                    if (node.style.display != 'block')
-                        node.style.display = 'block'
-                }
-            }
-        }
-    }
 </script>
 
 <Loadable loader={dynamicImport} {params} {delay}>
-    <div slot="loading">{isPageChanged.set(false) ? '' : ''}</div>
-    <div
-        class="loadable-container"
-        out:flashPrevent
-        slot="success"
-        let:component
-    >
+    <div slot="loading" />
+    <div class="loadable-container" slot="success" let:component>
         <svelte:component this={component} {params} />
-        {isPageChanged.set(true) ? '' : ''}
     </div>
-
 </Loadable>
-
-<style>
-    .loadable-container {
-        width: 100%;
-        height: 100%;
-        position: absolute;
-    }
-</style>
